d91a2464bbe15eae1de7a930219dc13dd77e8d1a
Kim-YH-handong <82792998+Kim-YH-handong@users.noreply.github.com>
2022-05-26 11:27:55 +0900

    Changed05/26

--- Todo.py
+++ Todo.py
@@ -1,7 +1,25 @@
 import tkinter
 import random
 from tkinter import messagebox
+from datetime import date
 
+tasks = []
+
+
+def sort_asc():
+    yesV = list()
+    noV = list()
+    for task in tasks:
+        if "[V]" in task:
+            yesV.append(task)
+        else:
+            noV.append(task)
+    delete_everything()
+    for i in noV:
+        tasks.append(i)
+    for i in yesV:
+        tasks.append(i)
+    update_tasks()
 
 def update_tasks():
     clear_listbox()
@@ -19,36 +37,40 @@ def add_task():
     label_dsply["text"] = ""
     Ntask = text_input.get()
     if Ntask != "":
+        Ntask = Ntask + "[  ]"
         tasks.append(Ntask)
         update_tasks()
     else:
-        label_dsply["text"] = "please enter the text"
+        label_dsply["text"] = "하려는 일을 입력하세요!"
     text_input.delete(0, 'end')
 
+def delete_everything():
+    global tasks
+    tasks = []
+    update_tasks()
 
 def delete_all():
     conf = messagebox.askquestion(
-        'delet all??', 'are you sure to delete all task?')
+        'delete [V] ?', 'are you sure to delete [V] tasks?')
     print(conf)
     if conf.upper() == "YES":
-        global tasks
-        tasks = []
+        for task in tasks:
+            if "[V]" in task:
+                tasks.remove(task)
+        # global tasks
+        # tasks = []
         update_tasks()
     else:
         pass
 
 
-def delete_one():
+def finish_one():
     de = lb_tasks.get("active")
     if de in tasks:
-        tasks.remove(de)
+        idx = de.find("[")
+        finish = de[:idx] + "[V]"
+        tasks[tasks.index(de)] = finish
     update_tasks()
-
-
-def sort_asc():
-    tasks.sort()
-    update_tasks()
-
 
 def sort_dsc():
     tasks.sort(reverse=True)
@@ -78,7 +100,7 @@ def save_act():
 
 def load_info():
     messagebox.showinfo(
-        "info", "this is Todolist V.1.0 \n created by FAIZAR RAHMAN \n Python Project",)
+        "showinfo", "OSSProject\nOriginal: FAIZAR RAHMAN\n Modified: KimYounghun")
 
 
 def load_act():
@@ -109,18 +131,15 @@ def exit_app():
 root = tkinter.Tk()
 # change root background col and ect
 root.configure(bg="white")
-root.title("my to do list")
-root.geometry("260x300")
+root.title("TODOLIST 프로그램")
+root.geometry("400x400")
 # database
-tasks = []
-# tasks = ['tes 1', 'best2', 'dest3']
+
 
 
 # GUI (graphical user interface)
 # main root app
-
-
-label_title = tkinter.Label(root, text="Todo List", bg="white")
+label_title = tkinter.Label(root, text="Todo List Output: ", bg="white")
 label_title.grid(row=0, column=0)
 
 label_dsply = tkinter.Label(root, text="", bg="white")
@@ -134,18 +153,18 @@ text_input.grid(row=1, column=1)
 
 # button section
 text_add_bttn = tkinter.Button(
-    root, text="add todo", bg="white", fg="green", width=15, command=add_task)
+    root, text="TODO 추가", bg="white", fg="green", width=15, command=add_task)
 text_add_bttn.grid(row=1, column=0)
 
 delone_bttn = tkinter.Button(
-    root, text="Done Task", bg="white", width=15, command=delete_one)
+    root, text="TODO 완료", bg="white", width=15, command=finish_one)
 delone_bttn.grid(row=2, column=0)
 
 delall_bttn = tkinter.Button(
-    root, text="Delete all", bg="white", width=15, command=delete_all)
+    root, text="[V] 삭제", bg="white", width=15, command=delete_all)
 delall_bttn.grid(row=3, column=0)
 
-sort_asc = tkinter.Button(root, text="sort (ASC)",
+sort_asc = tkinter.Button(root, text="[V]기준 정렬",
                           bg="White", width=15, command=sort_asc)
 sort_asc.grid(row=4, column=0)
 
@@ -154,27 +173,27 @@ sort_dsc = tkinter.Button(root, text="sort (DSC)",
 sort_dsc.grid(row=5, column=0)
 
 random_bttn = tkinter.Button(
-    root, text="random task", bg="White", width=15, command=random_task)
+    root, text="랜덤 TODO", bg="White", width=15, command=random_task)
 random_bttn.grid(row=6, column=0)
 
 number_task = tkinter.Button(
-    root, text="Number of Task", bg="white", width=15, command=number_task)
+    root, text="TODO 갯수", bg="white", width=15, command=number_task)
 number_task.grid(row=7, column=0)
 
-exit_bttn = tkinter.Button(root, text="exit app",
+exit_bttn = tkinter.Button(root, text="종료",
                            bg="white", width=15, command=exit_app)
 exit_bttn.grid(row=8, column=0)
 
 save_button = tkinter.Button(
-    root, text="save TodoList", bg="white", width=15, command=save_act)
+    root, text="TODO 저장", bg="white", width=15, command=save_act)
 save_button.grid(row=10, column=1)
 
 load_button = tkinter.Button(
-    root, text="Load LastTodolist", bg="white", width=15, command=load_act)
+    root, text="TODO 가져오기", bg="white", width=15, command=load_act)
 load_button.grid(row=10, column=0)
 
 info_button = tkinter.Button(
-    root, text="info", bg="white", width=15, command=load_info)
+    root, text="정보", bg="white", width=15, command=load_info)
 info_button.grid(row=11, column=0, columnspan=2)
 
 lb_tasks = tkinter.Listbox(root)
